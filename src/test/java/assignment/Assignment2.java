05a02b080183d564c19039414c2403390249e527
user1 <user1@gmail.com>
2023-12-08 09:59:49 -0800

    Assignment2

---
+++ src/test/java/assignment/Assignment2.java
@@ -0,0 +1,11 @@
+package assignment;
+
+public class Assignment2 {
+	
+	static String name = "Kushank";
+	public static void main(String[] args) {
+		System.out.println("This is first line of code");
+		System.out.println("user added the code" + name);
+	}
+
+}
